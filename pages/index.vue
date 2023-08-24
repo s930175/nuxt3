b7b9497a942120cbd58690b8eafe018d5efd7376
smile <s930175@gmail.com>
2023-08-24 11:22:03 +0800

    plugins注入

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,13 @@
 //use pinia
 const store = useHomeStore();
 const name = ref("mike");
+//調用plugins注入的方法
+const { $hello } = useNuxtApp();
 </script>
 <template>
   首頁:{{ store.count }}
   {{ name }}
+  <h2>{{ $hello("Kitty") }}</h2>
   <Home></Home>
   <!-- 自動找資料夾裡的index.vue -->
   <button @click="store.add">click</button>
