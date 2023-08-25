764ffca0f09319e9c759512d5c12211968abe1a5
smile <s930175@gmail.com>
2023-08-25 15:46:51 +0800

    meta SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  "app": {
+    "head": {
+      "viewport": "width=500, initial-scale=1",
+      "title": "Nuxt3 高效入門全攻略",
+      "meta": [
+          { "name": "description", "content": "這是 Mike 的 Nuxt3 高效入門全攻略課程" },
+    { "property": "og:title", "content": "Nuxt3 高效入門全攻略" },
+    { "property": "og:url", "content": "http://localhost:3000/" },
+    { "property": "og:description", "content": "這是 Mike 的 Nuxt3 高效入門全攻略課程" },
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        { src: 'https://awesome-lib.js' }
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+      ],
+      // please note that this is an area that is likely to change
+      // style: [
+      //   // <style type="text/css">:root { color: red }</style>
+      //   { children: ':root { color: red }', type: 'text/css' }
+      // ],
+      noscript: [
+        // <noscript>JavaScript is required</noscript>
+        { children: 'JavaScript is required' }
+      ]
+    }
+  }, 
   //指定某個資料夾auto import
   "components": {
     "dirs": [
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,20 @@
 <script setup>
-import AboutHeader from "@Component/About/header.vue";
-import AboutFooter from "@Component/About/footer.vue";
+// import AboutHeader from "@components/About/Header.vue";
+// import AboutFooter from "@components/About/Footer.vue";
+//局部SEO meta
+useHead({
+  title: "關於我們 About - Nuxt3 高效入門全攻略",
+  meta: [
+    { property: "og:title", content: "關於我們 About - Nuxt3 高效入門全攻略" },
+    { property: "og:url", content: "http://localhost:3000/about" },
+    { property: "og:image", content: "http://localhost:3000/share.jpg" },
+    { name: "description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
+    {
+      property: "og:description",
+      content: "關於我們 About - 最棒的Nuxt3的線上課程",
+    },
+  ],
+});
 </script>
 <template>
   <AboutHeader></AboutHeader>
