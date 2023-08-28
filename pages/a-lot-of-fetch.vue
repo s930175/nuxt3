cb53b6681f5ad5ec5c99c7a3f50f36c49e463034
smile <s930175@gmail.com>
2023-08-28 15:22:13 +0800

    會在 Promise.all 一起執行，等資料都跑完再一起回傳(一次打多個API使用)

---
+++ pages/a-lot-of-fetch.vue
@@ -0,0 +1,21 @@
+<script setup>
+// https://api.github.com/orgs/nuxt
+// https://api.github.com/orgs/nuxt/repos
+// const { data: orgsData } = await useFetch("https://api.github.com/orgs/nuxt");
+// const { data: reposData } = await useFetch(
+//   "https://api.github.com/orgs/nuxt/repos"
+// );
+
+//優化，一次打多個API
+const [{ data: orgsData }, { data: reposData }] = await Promise.all([
+  useFetch(`https://api.github.com/orgs/nuxt`),
+  useFetch(`https://api.github.com/orgs/nuxt/repos`),
+]);
+</script>
+<template>
+  <div>a lot of fetch</div>
+  {{ orgsData }}
+  {{ reposData }}
+</template>
+
+<style scoped></style>
